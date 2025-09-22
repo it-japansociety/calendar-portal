c1639aaa91fca2344c685f074d32c28767fd5add
it-japansociety <it@japansociety.org>
2025-09-22 11:54:25 -0400

    Updated Weekly Calendar Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -306,10 +306,10 @@ export default function Home() {
                   isDark ? 'bg-gray-800 border-red-800/30' : 'bg-white border-red-200'
                 }`}
               >
-                {/* Added by Robert */}
+                {/* Created by Max (Excel-Sharepoint Weekly Calendar) */}
                 <div style={{ position: "relative", width: "100%", paddingBottom: "65%", height: 0 }}>
                   <iframe
-                    src="https://japansoc.sharepoint.com/sites/JapanSociety/_layouts/15/Doc.aspx?sourcedoc={c689ad3e-8cd1-45fc-9e9b-2670eb7fddb0}&action=embedview&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdInConfigurator=True"
+                    src="https://japansoc.sharepoint.com/sites/JapanSociety/_layouts/15/Doc.aspx?sourcedoc={c689ad3e-8cd1-45fc-9e9b-2670eb7fddb0}&action=embedview&wdAllowInteractivity=False&Item='This%20Week'!A24&wdHideGridlines=True&wdDownloadButton=True&wdHideHeaders=True&wdInConfigurator=True&wdInConfigurator=True"
                     style={{
                       position: "absolute",
                       top: 0,
@@ -321,7 +321,7 @@ export default function Home() {
                   />
                 </div>
 
-                {/* Created by Max (Excel-Sharepoint Weekly Calendar) */}
+                {/* Modified by Robert */}
               </div>
 
           </section>
